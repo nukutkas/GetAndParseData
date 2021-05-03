fd783c9c19db91d2db37a0eebac9eaf2223b0787
Nikita Kochetov <nothsmple@mail.ru>
2021-05-03 13:53:48 +0300

    Initial Commit

---
+++ Networking/TableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  TableViewCell.swift
+//  Networking
+//
+//  Created by Татьяна Кочетова on 03.05.2021.
+//  Copyright © 2021 Nikita Kochetov. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell: UITableViewCell {
+
+    @IBOutlet var courseImage: UIImageView!
+    @IBOutlet var courseNameLabel: UILabel!
+    @IBOutlet var numberOfLessons: UILabel!
+    @IBOutlet var numberOfTests: UILabel!
+}
