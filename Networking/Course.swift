09984a932d01b87a3c4d77dc16891d785f3d91a2
Nikita Kochetov <nothsmple@mail.ru>
2021-05-03 14:06:24 +0300

    struct Course

---
+++ Networking/Course.swift
@@ -0,0 +1,18 @@
+//
+//  Course.swift
+//  Networking
+//
+//  Created by Татьяна Кочетова on 03.05.2021.
+//  Copyright © 2021 Nikita Kochetov. All rights reserved.
+//
+
+import Foundation
+
+struct Course {
+    let id: Int
+    let name: String
+    let link: String
+    let imageUrl: String
+    let number_of_lessons: Int
+    let number_of_tests: Int
+}
