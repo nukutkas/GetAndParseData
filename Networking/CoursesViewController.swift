bd4dbb57f8846abcb09163f9b1292616fc893c87
Nikita Kochetov <nothsmple@mail.ru>
2021-05-03 17:34:41 +0300

    Get and parse data

--- Networking/CoursesViewController.swift
+++ Networking/CoursesViewController.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 class CoursesViewController: UIViewController {
+    
+    private var courses = [Course]()
+    private var courseName: String?
+    private var courseURL: String?
 
     @IBOutlet var tableView: UITableView!
     
@@ -22,9 +26,9 @@ class CoursesViewController: UIViewController {
     func fetchData() {
         
        // let jsonUrlString = "https://swiftbook.ru/wp-content/uploads/api/api_course"
-       // let jsonUrlString = "https://swiftbook.ru/wp-content/uploads/api/api_courses"
+        let jsonUrlString = "https://swiftbook.ru/wp-content/uploads/api/api_courses"
        // let jsonUrlString = "https://swiftbook.ru/wp-content/uploads/api/api_website_description"
-        let jsonUrlString = "https://swiftbook.ru/wp-content/uploads/api/api_missing_or_wrong_fields"
+       // let jsonUrlString = "https://swiftbook.ru/wp-content/uploads/api/api_missing_or_wrong_fields"
         
         
         guard let url = URL(string: jsonUrlString) else { return }
@@ -34,8 +38,15 @@ class CoursesViewController: UIViewController {
             guard let data = data else { return }
             
             do {
-                let websiteDescription = try JSONDecoder().decode(WebsiteDescription.self, from: data)
-                print("\(websiteDescription.websiteName ?? "") \(websiteDescription.websiteDescription ?? "")")
+                let decoder = JSONDecoder()
+                decoder.keyDecodingStrategy = .convertFromSnakeCase
+                
+                self.courses = try decoder.decode([Course].self, from: data)
+                
+                DispatchQueue.main.async {
+                    self.tableView.reloadData()
+                }
+                
             } catch let error {
                 print("Error serialization json", error)
             }
@@ -43,15 +54,46 @@ class CoursesViewController: UIViewController {
             
         }.resume()
     }
-    /*
+    
+    private func configureCell(cell: TableViewCell, for indexpath: IndexPath) {
+        
+        let course = courses[indexpath.row]
+        cell.courseNameLabel.text = course.name
+        
+        if let numberOfLessons = course.numberOfLessons {
+            cell.numberOfLessons.text = "Number of lessons: \(numberOfLessons)"
+        }
+        
+        if let numberOfTests = course.numberOfTests {
+            cell.numberOfTests.text = "Number of tests: \(numberOfTests)"
+        }
+        
+        DispatchQueue.global().async {
+            guard let imageUrl = URL(string: course.imageUrl!) else { return }
+            guard let imageData = try? Data(contentsOf: imageUrl) else { return }
+            
+            DispatchQueue.main.async {
+                cell.courseImage.image = UIImage(data: imageData)
+            }
+        }
+        
+    }
+    
+    
+    
+    
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        let webViewController = segue.destination as! WebViewController
+        webViewController.selectedCourse = courseName
+        
+        if let url = courseURL {
+            webViewController.courseURL = url
+        }
     }
-    */
+    
 
 }
 
@@ -60,12 +102,14 @@ class CoursesViewController: UIViewController {
 extension CoursesViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return courses.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell") as! TableViewCell
+        
+        configureCell(cell: cell, for: indexPath)
         
         return cell
     }
@@ -77,6 +121,15 @@ extension CoursesViewController: UITableViewDelegate {
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 100
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let course = courses[indexPath.row]
+        
+        courseURL = course.link
+        courseName = course.name
+        
+        performSegue(withIdentifier: "Description", sender: self)
     }
 }
 
