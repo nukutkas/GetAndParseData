e237e739c1a8c9992156f14a563307463793a3e2
Nikita Kochetov <nothsmple@mail.ru>
2021-05-03 19:49:00 +0300

    CollectionView for the buttons

---
+++ Networking/CollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  CollectionViewCell.swift
+//  Networking
+//
+//  Created by Татьяна Кочетова on 03.05.2021.
+//  Copyright © 2021 Nikita Kochetov. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var label: UILabel!
+    
+}
---
+++ Networking/MainViewController.swift
@@ -0,0 +1,34 @@
+//
+//  MainViewController.swift
+//  Networking
+//
+//  Created by Татьяна Кочетова on 03.05.2021.
+//  Copyright © 2021 Nikita Kochetov. All rights reserved.
+//
+
+import UIKit
+
+private let reuseIdentifier = "Cell"
+
+class MainViewController: UICollectionViewController {
+    
+    let actions = ["Download Image", "GET", "POST", "OUR COURSES", "Upload Image"]
+
+
+
+    // MARK: UICollectionViewDataSource
+
+    override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return actions.count
+    }
+
+    override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! CollectionViewCell
+    
+        cell.label.text = actions[indexPath.row]
+        return cell
+    }
+
+    // MARK: UICollectionViewDelegate
+
+}
