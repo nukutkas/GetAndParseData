01e72a8e812a4a3a5eba08446f2ace55b97a776b
Nikita Kochetov <nothsmple@mail.ru>
2021-05-08 21:56:05 +0300

    Download Progress

--- Networking/Services/AlamofireNetworkRequest.swift
+++ Networking/Services/AlamofireNetworkRequest.swift
@@ -11,6 +11,9 @@ import Alamofire
 
 class AlamofireNetworkRequest {
     
+    static var onProgress: ((Double) -> ())?
+    static var completed: ((String) -> ())?
+    
     static func sendRequest(url: String, completion: @escaping (_ courses: [Course])->()) {
         
         guard let url = URL(string: url) else { return }
@@ -25,6 +28,22 @@ class AlamofireNetworkRequest {
                 courses = Course.getArray(from: value)!
                 completion(courses)
                 
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+    
+    static func downloadImage(url: String, completion: @escaping (_ image: UIImage)->()) {
+        
+        guard let url = URL(string: url) else { return }
+        
+        AF.request(url).responseData { (responseData) in
+            
+            switch responseData.result {
+            case .success(let data):
+                guard let image = UIImage(data: data) else { return }
+                completion(image)
             case .failure(let error):
                 print(error)
             }
@@ -48,9 +67,9 @@ class AlamofireNetworkRequest {
             
             switch responseString.result {
             case .success(let string):
-            print(string)
+                print(string)
             case .failure(let error):
-            print(error)
+                print(error)
             }
         }
     }
@@ -62,9 +81,35 @@ class AlamofireNetworkRequest {
             guard
                 let data = response.data,
                 let string = String(data: data, encoding: .utf8)
-                else { return }
+            else { return }
             
             print(string)
         }
+    }
+    
+    static func downloadImageWithProgress(url: String, completion: @escaping (_ image: UIImage) -> ()) {
+        guard let url = URL(string: url) else { return }
+        
+        AF.request(url)
+            .validate()
+            .downloadProgress { (progress) in
+                
+                print("totalUnitCount: \(progress.totalUnitCount)\n")
+                print("completedUnitCount:\(progress.completedUnitCount)\n")
+                print("fractionCompleted:\(progress.fractionCompleted)\n")
+                print("loclizedDescription:\(progress.localizedDescription!)\n")
+                print("---------------------------------------------------------")
+                
+                self.onProgress?(progress.fractionCompleted)
+                self.completed?(progress.localizedDescription)
+                
+            }.response { (response) in
+                
+                guard let data = response.data, let image = UIImage(data: data) else { return }
+                
+                DispatchQueue.main.async {
+                    completion(image)
+                }
+            }
     }
 }
--- Networking/ViewControllers/ImageViewController.swift
+++ Networking/ViewControllers/ImageViewController.swift
@@ -21,38 +21,41 @@ class ImageViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        activityIndicator.isHidden = true
+        activityIndicator.startAnimating()
         activityIndicator.hidesWhenStopped = true
-        fetchImage()
         completedLabel.isHidden = true
         progressView.isHidden = true
     }
     
     func fetchImage() {
         
-        activityIndicator.isHidden = false
-        activityIndicator.startAnimating()
-        
         NetworkManager.downloadImage(url: url) { (image) in
-            self.activityIndicator.stopAnimating()
             self.imageView.image = image
         }
     }
     func fetchDataWithAlamofire() {
-        AF.request(url).responseData { (responseData) in
-            switch responseData.result {
-            case .success(let data):
-                guard let image = UIImage(data: data) else { return }
-                
-                self.activityIndicator.stopAnimating()
-                self.imageView.image = image
-
-            case .failure(let error):
-                print(error)
-            }
+        AlamofireNetworkRequest.downloadImage(url: url) { (image) in
+            
+            self.activityIndicator.stopAnimating()
+            self.imageView.image = image
         }
     }
     func downloadImageWithProgress() {
+        AlamofireNetworkRequest.onProgress = { progress in
+            self.progressView.isHidden = false
+            self.progressView.progress = Float(progress)
+        }
         
+        AlamofireNetworkRequest.completed = { completed in
+            self.completedLabel.isHidden = false
+            self.completedLabel.text = completed
+        }
+        
+        AlamofireNetworkRequest.downloadImageWithProgress(url: largeImageUrl) { (image) in
+            self.activityIndicator.stopAnimating()
+            self.completedLabel.isHidden = true
+            self.progressView.isHidden = true
+            self.imageView.image = image
+        }
     }
 }
