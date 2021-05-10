25974c3f95ae706d0f0d75bd194a08c2167c9210
Nikita Kochetov <nothsmple@mail.ru>
2021-05-10 22:15:42 +0300

    Fix to number of

--- Networking/Services/AlamoFireNetworkRequest.swift
+++ Networking/Services/AlamoFireNetworkRequest.swift
@@ -120,8 +120,8 @@ class AlamofireNetworkRequest {
         let userData: [String: Any] = ["name" : "Network Request",
                                        "link" : "https://swiftbook.ru/contents/our-first-applications/",
                                        "imageUrl" : "https://swiftbook.ru/wp-content/uploads/sites/2/2018/08/notifications-course-with-background.png",
-                                       "numberOfLessons" : 18,
-                                       "numberOfTests" : 10]
+                                       "numberOfLessons" : "18",
+                                       "numberOfTests" : "10"]
         AF.request(url, method: .post, parameters: userData).responseJSON { (responseJSON) in
             
             guard let statusCode = responseJSON.response?.statusCode else { return }
@@ -154,8 +154,8 @@ class AlamofireNetworkRequest {
         let userData: [String: Any] = ["name" : "Network Request with Alamofire",
                                        "link" : "https://swiftbook.ru/contents/our-first-applications/",
                                        "imageUrl" : "https://swiftbook.ru/wp-content/uploads/sites/2/2018/08/notifications-course-with-background.png",
-                                       "numberOfLessons" : 18,
-                                       "numberOfTests" : 10]
+                                       "numberOfLessons" : "18",
+                                       "numberOfTests" : "10"]
         AF.request(url, method: .put, parameters: userData).responseJSON { (responseJSON) in
             
             guard let statusCode = responseJSON.response?.statusCode else { return }
