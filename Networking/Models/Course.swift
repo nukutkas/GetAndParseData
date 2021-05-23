1703baab6133f505803e4c15e3069383151babee
Nikita Kochetov <nothsmple@mail.ru>
2021-05-23 17:21:46 +0300

    Course model changes.

--- Networking/Models/Course.swift
+++ Networking/Models/Course.swift
@@ -8,35 +8,22 @@
 
 import Foundation
 
-/*
- struct Course: Decodable {
- 
- let id: Int?
- let name: String?
- let link: String?
- let imageUrl: String?
- let numberOfLessons: Int?
- let numberOfTests: Int?
- }
- */
-
-
 struct Course: Decodable {
     
     let id: Int?
     let name: String?
     let link: String?
     let imageUrl: String?
-    let numberOfLessons: String?
-    let numberOfTests: String?
+    let numberOfLessons: Int?
+    let numberOfTests: Int?
     
     init?(json: [String: Any]) {
         let id = json["id"] as? Int
         let name = json["name"] as? String
         let link = json["link"] as? String
         let imageUrl = json["imageUrl"] as? String
-        let numberOfLessons = json["numberOfLessons"] as? String
-        let numberOfTests = json["numberOfTests"] as? String
+        let numberOfLessons = json["numberOfLessons"] as? Int
+        let numberOfTests = json["numberOfTests"] as? Int
         
         self.id = id
         self.name = name
